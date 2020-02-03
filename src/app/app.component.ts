cbe96ec8c4339b5516298c13f19eeb8441c291d6
Kélvin Santiago <kelvinsleonardo@gmail.com>
2020-02-03 07:08:34 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {RxwebValidators} from '@rxweb/reactive-form-validators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rxweb';
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      products: this.fb.array([this.createItem()]),
+    });
+  }
+
+  createItem() {
+    return this.fb.group({
+      price: '',
+      newPrice: ['', [RxwebValidators.lessThanEqualTo({fieldName: 'price'})]],
+    });
+  }
+
+  addItem() {
+    const fa = this.form.controls.products as FormArray;
+    fa.push(this.createItem());
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RxReactiveFormsModule} from '@rxweb/reactive-form-validators';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RxReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
